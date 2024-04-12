f34eb5cbe9a54a8d5586944a5f236a378c1d2ac7
= <Selenac99@hotmail.com>
2024-04-12 17:09:56 -0300

    terminando banner

--- src/components/SearchLocation/SearchLocation.tsx
+++ src/components/SearchLocation/SearchLocation.tsx
@@ -5,7 +5,7 @@ export function SearchLocation() {
     const [isOpen, setIsOpen] = useState(false)
 
     return (
-        <div className="relative mb-2 md:mb-0 flex items-center gap-4 border-[1px] rounded-lg px-3 py-2 justify-between cursor-pointer">
+        <div className="relative mb-2 md:mb-0 flex items-center gap-4 border-[1px] rounded-lg px-3 py-2 justify-between cursor-pointer" onClick={() => setIsOpen(!isOpen)}>
 
 
             <GrLocation />
@@ -18,6 +18,13 @@ export function SearchLocation() {
                 <GrFormUp />
             ) : (
                 <GrFormDown />
+            )}
+            {isOpen && (
+                <div className="absolute top-[70px] bg-white z-50 p-4 rounded-lg shadow-light w-[230px] left-0">
+                    <p>Madrid</p>
+                    <p>Barcelona</p>
+                    <p>Zaragoza</p>
+                </div>
             )}
 
         </div>
