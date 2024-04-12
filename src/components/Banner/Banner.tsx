b2d5566b6472790be079f03f71cceda8944a1efc
= <Selenac99@hotmail.com>
2024-04-12 16:58:31 -0300

    me marca error y no lo encuentro

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,5 +1,18 @@
+import { FloatedSearch } from "../FloatedSearch";
 export function Banner() {
-    return (
-        <p>Banner</p>
-    )
+  return (
+    <div className="container relative mx-auto">
+      <div className="pt-20 md:pt-0 min-h-[80vh] bg-banner bg-no-repeat bg-cover bg-center rounded-3xl flex flex-col items-center md:justify-center">
+        <div className="max-w-3xl text-center text-white">
+          <h2 className="text-5xl font-semibold">
+            Descubre la vivienda ideal para tu familia
+          </h2>
+          <p className="mt-2 text-xl md:mt-8">
+            Con más de 30 años de experiencia{" "}
+          </p>
+        </div>
+        <FloatedSearch />
+      </div>
+    </div>
+  );
 }
--- src/components/FloatedSearch/FloatedSearch.tsx
+++ src/components/FloatedSearch/FloatedSearch.tsx
@@ -1,6 +1,21 @@
+import {Transition} from '../Transition';
+import {SearchLocation} from '../SearchLocation'
+import {SearchProperty} from '../SearchProperty'
+import {SearchPriceRange} from '../SearchPriceRange'
+import { SearchButton } from '../SearchButton';
+
+
 
 export function FloatedSearch() {
     return (
-        <p>Floated search</p>
+        <Transition className="absolute bottom-10 md:-bottom-10 left-0 right-0 w-[75%] mx-auto">
+        <div className="flex-col justify-between gap-4 py-4 bg-white rounded-md md:flex md:flex-row backdrop-blur shadow-light">
+            <SearchLocation />
+            <SearchProperty />
+            <SearchPriceRange />
+            <SearchButton />
+
+        </div>
+        </Transition>
     )
-}
+} 
